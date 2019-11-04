06d93b0b79300b3f33763581787e13f12de2bce5
ahargey <3650393@myuwc.ac.za>
2019-11-04 12:12:55 +0200

    #SAC adjusted

--- SAC.R
+++ SAC.R
@@ -1,7 +1,7 @@
 #AYESHA HARGEY
 #3650393
 #17TH OCTOBER 2019
-#CAMTRAPR TESTING
+#SAC TESTING
 
 #LIBRARIES
 library(tidyverse)
@@ -10,20 +10,20 @@ library(iNEXT)
 library(vegan)
 
 #DATA LOADED
-sac <- read.csv("Datasets/Animals/SAC_Aggregation_CSV.csv")
-all_sites_sac <- read.csv("Datasets/Animals/SAC_All_Species_CSV.csv")
+sac <- read.csv("Datasets/Animals/SAC/SAC_Aggregation_CSV.csv")
+all_sites_sac <- read.csv("Datasets/Animals/SAC/SAC_All_Species_CSV.csv")
 
 all_sites_sac <- select(all_sites_sac, -Date)
 
 curve <- read_delim("CURVES ONLY.csv",
                     ";", escape_double = FALSE,
-                    trim_ws = TRUE)
+                    trim_ws = TRUE) #bryan's code
 
 curve.main = curve[1:38,15:24] #bryan's code
 curve.data = curve[3:12] #bryan's code
 
-sac_main = sac[,3:16] #removing date columns
-sac_site = sac[,3:16]
+sac_main = sac[,3:16] #removing date columns #bryan's code
+sac_site = sac[,3:16] #bryan's code
 
 curve_all_sites = specaccum(all_sites_sac, method = "exact")
 
@@ -46,7 +46,7 @@ curve_NPR3 = specaccum(NPR3, method = "exact")
 #plot curve_all first
 plot(curve_all_sites,
      xlab = "Months",
-     ylab = "Species richness")
+     ylab = "Species (N)")
 #then plot the rest
 plot(curve_NPR1, add = TRUE, ci = 0, col="hotpink", lwd=2) #col is COLOUR setting, so change it to something else if you want
 plot(curve_NPR2, add = TRUE, ci = 0, col = "orchid4", lwd = 2)
@@ -54,3 +54,4 @@ plot(curve_NPR3, add = TRUE,ci = 0, col = "lightblue", lwd = 2)
 
 legend(8, 6, legend=c("NPR1", "NPR2", "NPR3"),
        col=c("hotpink", "orchid4", "lightblue"), lty=1)
+
